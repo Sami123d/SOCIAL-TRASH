3020b43dfe816c15f13b073bcc4fb39b3386d80d
Sami <saminazirqureshi1@gmail.com>
2024-04-15 01:18:11 +0500

    on working

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import Home from "./Pages/homess/Home.jsx";
+import post
+function App() {
+  return (
+    <>
+      
+      <AcUnitIcon />
+      <p>SAMI</p>
+      <Home />
+      <post/>
+    </>
+  );
+}
+
+export default App;
